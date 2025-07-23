9a64752579ed254e663f55b2cbf927dfb4bb4bb3
Xavier Maso <xavier.maso@beta.gouv.fr>
2025-07-23 15:17:38 +0200

    [DATAENG] Exemple de test qui échoue à corriger avec de l'injection de dépendences

--- profil-data-engineer/src/main.py
+++ profil-data-engineer/src/main.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import Depends, FastAPI
 
 app = FastAPI()
 
+
+class FournisseurDeServices():
+    def dis_bonjour(self):
+        return "Bonjour le monde"
+
+
+def instancie_les_services():
+    return FournisseurDeServices()
+
+
 @app.get("/bonjour")
-def bonjour():
-    return "Bonjour le monde"
+def bonjour(services: Annotated[str, Depends(instancie_les_services)]):
+    message = services.dis_bonjour()
+    return message
